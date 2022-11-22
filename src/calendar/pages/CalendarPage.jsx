b4e5ee166e569ef3c02d0961b5dd63651b78acc3
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-22 19:08:41 -0300

    se agregan config adcionales al calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,27 +1,13 @@
 import React from 'react'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
+
 
 import { NavBar } from '../components/NavBar';
 import { addHours } from 'date-fns/esm';
+import { localizer ,getMessagesES} from '../../helper';
 
 
-const locales = {
-    'en-US': enUS,
-  }
-
-  const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-  })
 
   const events = [{
     title:'Cumple de Lola',
@@ -36,17 +22,29 @@ const locales = {
   }]
 export const CalendarPage = () => {
 
-    
+    const eventStyleGetter = (event,start,end,isSelected) => {
+        console.log({event,start,end,isSelected});
+
+        const style = {
+            backgroundColor:'#347CF7',
+            borderRadius:'0px',
+            opacity:0.8,
+            color:'white'
+        }
+    }
 
   return (
     <div>
         <NavBar />
         <Calendar
+        culture='es'
       localizer={ localizer }
       events={ events }
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
+      messages={ getMessagesES() }
+      eventPropGetter={ eventStyleGetter }
     />
     </div>
   )
