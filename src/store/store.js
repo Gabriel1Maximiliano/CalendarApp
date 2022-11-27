88c3624d3fd1c92544619b7a21a976a77a8e5eeb
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-27 12:45:27 -0300

    It is added calenarSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/authSlice";
+
 import { calendarSlice } from "./calendar/calendarSlice";
 import {uiSlice} from "./ui/uiSlice";
 
@@ -9,5 +11,10 @@ export const store = configureStore({
     reducer:{
         ui:uiSlice.reducer,
         calendar: calendarSlice.reducer,
-    }
+        auth: authSlice.reducer
+    },
+    middleware:(getDefaultMiddleware
+        )=>getDefaultMiddleware({
+            serializableCheck:false
+        })
 }) 
