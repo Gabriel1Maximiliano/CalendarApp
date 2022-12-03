bf2154d9bb55629f2b403230de43c6e39c9dca7c
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-03 12:49:19 -0300

    It is added calendar states fixtures

---
+++ tests/fixtures/calendarStates.js
@@ -0,0 +1,40 @@
+
+
+export const events =[{
+
+    id: '1',
+    start: new Date('2022-10-21 13:00:00'),
+    end: addHours('2022-10-21 15:00:00'),
+    title: 'Cumpleaños de Lola',
+    notes: 'Hay que comprar el pastel',
+},
+{
+
+   id: '2',
+   start: new Date('2022-11-21 13:00:00'),
+   end: addHours('2022-11-21 15:00:00'),
+   title: 'Cumpleaños de Martina',
+   notes: 'Hay que comprar el pasteluna remera de Henry'
+}] 
+
+export const initialState = {
+    isLoadingEvents: true,
+    events: [
+        // tempEvent
+    ],
+    activeEvent: null
+}
+export const calendarWithEventsState = {
+    isLoadingEvents: false,
+    events: [
+       ...events
+    ],
+    activeEvent: null
+}
+export const calendarWithActiveEventsState = {
+    isLoadingEvents: false,
+    events: [
+       ...events
+    ],
+    activeEvent: {...events[0]}
+}
