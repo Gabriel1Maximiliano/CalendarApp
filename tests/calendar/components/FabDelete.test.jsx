6c662dd368e58839546bf8eaa668049a58ed411a
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-05 12:00:55 -0300

    tests on < FabDelelte /> should display the component correctly ok

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,8 +1,20 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { FabDelete } from "../../../src/calendar/components/FabDelete";
+import { useCalendarStore } from "../../../src/hooks";
 
+jest.mock("../../../src/hooks");
 
 describe('tests on < FabDelelte />', () => { 
     test('should display the component correctly  ', () => {
-    //render( <FabDelete /> );
+  
+        useCalendarStore.mockReturnValue({
+            hasEventSelected:false
+        });
+    render( <FabDelete /> );
+    const btn = screen.getByLabelText('btn-delete');
+
+    expect( btn.classList ).toContain('btn');
+    expect( btn.classList ).toContain('btn-danger');
+    expect( btn.classList ).toContain('fab-danger');
+    expect( btn.style.display ).toBe('none');
 }); })
