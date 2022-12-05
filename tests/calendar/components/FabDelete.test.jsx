d08c3c8a8633ae2453b98cc39620312e8a794854
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-05 12:11:18 -0300

    < FabDelelte />  should show the button  if there is an active event

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,20 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { FabDelete } from "../../../src/calendar/components/FabDelete";
 import { useCalendarStore } from "../../../src/hooks";
 
 jest.mock("../../../src/hooks");
 
-describe('tests on < FabDelelte />', () => { 
-    test('should display the component correctly  ', () => {
-  
-        useCalendarStore.mockReturnValue({
-            hasEventSelected:false
-        });
-    render( <FabDelete /> );
-    const btn = screen.getByLabelText('btn-delete');
+describe('tests on < FabDelelte />', () => {
 
-    expect( btn.classList ).toContain('btn');
-    expect( btn.classList ).toContain('btn-danger');
-    expect( btn.classList ).toContain('fab-danger');
-    expect( btn.style.display ).toBe('none');
-}); })
+    const mockStartDeletingEvents = jest.fn();
+
+    beforeEach( () => jest.clearAllMocks() );
+
+
+    test('should display the component correctly  ', () => {
+
+        useCalendarStore.mockReturnValue({
+            hasEventSelected: false
+        });
+        render(<FabDelete />);
+        const btn = screen.getByLabelText('btn-delete');
+
+        expect(btn.classList).toContain('btn');
+        expect(btn.classList).toContain('btn-danger');
+        expect(btn.classList).toContain('fab-danger');
+        expect(btn.style.display).toBe('none');
+    });
+    test('should show the button  if there is an active event ', () => {
+        useCalendarStore.mockReturnValue({
+            hasEventSelected: true,
+            startDeletingEvent: mockStartDeletingEvents
+        });
+        render(<FabDelete />);
+        const btn = screen.getByLabelText('btn-delete');
+
+        fireEvent.click( btn );
+
+        expect( mockStartDeletingEvents ).toHaveBeenCalled();
+    })
+
+
+
+
+})
